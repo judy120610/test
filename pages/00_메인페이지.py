0df09e0b3c6302923e7023be8024fb215f7d084c
judy120610 <skyoung2178@gmail.com>
2026-01-10 00:01:55 +0900

    Create 00_메인페이지.py

---
+++ pages/00_메인페이지.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="즐거운 미니게임 천국", layout="wide")
+
+st.title("🎮 즐거운 미니게임")
+st.subheader("왼쪽 메뉴를 클릭해 미니게임을 선택하세요")
+
+st.markdown("""
+---
+이 사이트는 스트림릿으로 만든 미니게임 모음집입니다.  
+사이드바의 메뉴를 통해 원하는 게임을 즐겨보세요!
+""")
